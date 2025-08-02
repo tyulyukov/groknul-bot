09c639aa6b5790fe43ffb231ac59c55d08bfcd06
tyulyukov <makstyulyukov@gmail.com>
2025-08-02 15:09:32 +0300

    feat: enhance logger formatting and update dev script

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,15 +1,15 @@
 import pino from 'pino';
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss Z',
-      ignore: 'pid,hostname',
+export const logger = pino(
+  {
+    formatters: {
+      level: (label) => {
+        return { level: label };
+      },
     },
+    timestamp: pino.stdTimeFunctions.isoTime,
   },
-});
+  pino.destination({ sync: false }),
+);
 
-export default logger; 
+export default logger;
